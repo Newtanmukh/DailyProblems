70d848aa5f554f2da970e09d1470f30d9ba7a4b6
Newtanmukh <importantnewton@gmail.com>
2024-08-03 16:20:51 +0530

    GFG3rd August 2024 added

---
+++ Leetcode/MAKE TWO ARRAY EQUAL BY REVERSING SUBARRAYS/CODE.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean canBeEqual(int[] target, int[] arr) {
+        HashMap<Integer, Integer> hashMap = new HashMap<>();
+
+        for(int i=0; i<target.length; i++){
+            hashMap.put(target[i], hashMap.getOrDefault(target[i], 0) + 1);
+            hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0) - 1);
+        }
+
+        // for(int i=0; i<arr.length; i++){
+        //     hashMap.put(arr[i], hashMap.getOrDefault(arr[i], 0) - 1);
+        // }
+
+        for(Map.Entry<Integer, Integer> entry: hashMap.entrySet()){
+            if(entry.getValue()!=0)
+                return false;
+        }
+
+        return true;
+    }
+}
